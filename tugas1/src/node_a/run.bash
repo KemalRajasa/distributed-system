f1fd904b7ee02d9b815a8a9a209409858aab2f7a
Kemal Tangguh Aji Rajasa <162313398+KemalRajasa@users.noreply.github.com>
2025-11-02 22:29:57 +0700

    Remove offset-ms argument from run.bash

--- tugas1/src/node_a/run.bash
+++ tugas1/src/node_a/run.bash
@@ -3,5 +3,4 @@ python3 peer_node.py \
 --name A --listen 0.0.0.0 5000 \
 --peers A@192.168.122.39:5000 B@192.168.122.189:5001 D@192.168.122.159:5002 \
 --logger 192.168.122.7 9999 \
---offset-ms 600 \
 --initiate-broadcast --msg "Hello from A"
