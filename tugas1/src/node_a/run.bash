b2eda88d794caed7f7e3f10d49e317c5fac4b727
Kemal Tangguh Aji Rajasa <162313398+KemalRajasa@users.noreply.github.com>
2025-11-02 22:28:13 +0700

    Update run.bash

--- tugas1/src/node_a/run.bash
+++ tugas1/src/node_a/run.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 python3 peer_node.py \
-	  --name A --listen 0.0.0.0 5000 \
-	  --peers A@localhost:5000 B@localhost:5001 D@localhost:5002 \
-	  --logger localhost 9999 \
-	  --offset-ms 600 \
-	  --initiate-broadcast --msg "Hello from A"
+--name A --listen 0.0.0.0 5000 \
+--peers A@192.168.122.39:5000 B@192.168.122.189:5001 D@192.168.122.159:5002 \
+--logger 192.168.122.7 9999 \
+--offset-ms 600 \
+--initiate-broadcast --msg "Hello from A"
