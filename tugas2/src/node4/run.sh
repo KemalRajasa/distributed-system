5403c3b947f1b9c29b73de2083c3ffca6899667e
KemalRajasa <kemalptn2023@gmail.com>
2025-11-10 07:48:23 +0700

    revised node 4 and 5

--- tugas2/src/node4/run.sh
+++ tugas2/src/node4/run.sh
@@ -3,7 +3,7 @@
 PEERS="192.168.122.214:8001=1,192.168.122.200:8002=2,192.168.122.114:8004=4,192.168.122.68:8005=5"
 LOGGER="192.168.122.143:9000"
 
-python3 ./kv.py --id 3 --tcp 8003 --udp 8103 \
+python3 ./kv.py --id 4 --tcp 8004 --udp 8104 \
   --peers $PEERS \
   --logger-addr $LOGGER \
   --numnodes 5 \
