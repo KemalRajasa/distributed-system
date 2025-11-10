64619ce0cb795619b0dbfd36d4280f7e3a67fc3e
KemalRajasa <kemalptn2023@gmail.com>
2025-11-10 17:38:51 +0700

    revised tugas1 run.sh

--- tugas1/src/node_a/run.bash
+++ tugas1/src/node_a/run.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 peer_node.py \
-  --name A --listen 0.0.0.0 5000 \
-  --peers A@192.168.122.39:5000 B@192.168.122.189:5001 D@192.168.122.159:5002 \
-  --logger 192.168.122.7 9999 \
-  --initiate-broadcast --msg "Hello from A"
+--name A --listen 0.0.0.0 5000 \
+--peers A@192.168.122.168:5000 B@192.168.122.218:5001 D@192.168.122.153:5002 \
+--logger 192.168.122.193 9999 \
+--initiate-broadcast --msg "Hello from A"
